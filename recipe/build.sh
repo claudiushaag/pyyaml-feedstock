778f7f6d8d57ada43efd2c411c7e8811d445dc0c
Claudius Haag <claudius.haag@web.de>
2025-11-30 10:15:05 +0100

    Update build scripts b/c of depreciation of --global-options by pip 25.3 + disable 0001 patch - does not work...?

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 if [[ "$use_noarch" == "true" ]]; then
   PYYAML_FORCE_LIBYAML=0 ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
 else
-  ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv --global-option="--with-libyaml" --global-option="build_ext" --global-option="-I${PREFIX}/include" --global-option="-L${PREFIX}/lib"
+  PYYAML_FORCE_LIBYAML=1 ${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
 fi
